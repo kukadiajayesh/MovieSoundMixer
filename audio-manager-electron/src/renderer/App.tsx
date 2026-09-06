3c53e2381743b0e593d06ba8c46a04379cf9beae
Kukadia Jayesh <kukadia.jayesh@gmail.com>
2026-09-07 01:31:06 +0530

    Remove app title

--- audio-manager-electron/src/renderer/App.tsx
+++ audio-manager-electron/src/renderer/App.tsx
@@ -40,7 +40,7 @@ export default function App() {
       <div className="app">
         <div className="titlebar">
           <div className="tl-left-spacer" style={{ width: '200px' }}></div>
-          <div className="tl-title">FFmpeg Audio Manager{version ? ` — v${version}` : ''}</div>
+          <div className="tl-title"></div>
           <div className="tl-spacer" />
         </div>
 
